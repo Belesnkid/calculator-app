f7a32bec81407119a4cbba1a85bedafe11bd0ddb
Daniel J Belenski <60483819+DanielBelenski@users.noreply.github.com>
2021-12-27 11:34:58 -0500

    Subtract
    Wrote the subtract functionality.

--- calc.spec.ts
+++ calc.spec.ts
@@ -1,7 +1,13 @@
-import {add} from "./calc-functions"
+import {add, subtract} from "./calc-functions"
 describe("Calculator Tests", () =>{
+    
     it("Should add 2 + 2", ()=>{
         const sum = add(2,2);
         expect(sum).toBe(4);
+    });
+
+    it("Should subract 2 from 2", () =>{
+        const diff = subtract(2,2);
+        expect(diff).toBe(0);
     })
 })
--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import Express from "express";
-import { add } from "./calc-functions";
+import { add, subtract } from "./calc-functions";
 
 const app = Express();
 
-app.get('/add/:num1/:num2', (req,res)=>{
+app.get("/add/:num1/:num2", (req,res)=>{
     const {num1,num2} = req.params;
-    const sum = add(Number(num1),Number(num2))
-    res.status(200);
+    const sum = add(Number(num1),Number(num2));
+    res.status(201);
     res.send(sum);
+})
+
+app.get("/subtract/:num1/:num2", (req,res) =>{
+    const {num1,num2} = req.params;
+    const diff = subtract(Number(num1), Number(num2));
+    res.status(201);
+    res.send(diff);
 })
 
 app.listen(3000, () => console.log("App has started"));
