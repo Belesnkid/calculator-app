7557d0c576d8c019f991b443fbf3a554fc50331c
Daniel J Belenski <38082687+Belesnkid@users.noreply.github.com>
2021-12-28 10:40:05 -0500

    Update index.ts
    query params

--- index.ts
+++ index.ts
@@ -7,14 +7,23 @@ app.get("/add/:num1/:num2", (req,res)=>{
     const {num1,num2} = req.params;
     const sum = add(Number(num1),Number(num2));
     res.status(201);
-    res.send(sum);
+    res.send(String(sum));
 })
 
 app.get("/subtract/:num1/:num2", (req,res) =>{
     const {num1,num2} = req.params;
     const diff = subtract(Number(num1), Number(num2));
     res.status(201);
-    res.send(diff);
+    res.send(String(diff));
+})
+
+//query params not in route
+
+//localhost:3000/subtract?num1=100&num2=80
+app.get("/subtract", (req,res) =>{
+    const {num1, num2} = req.query;
+    const diff = subtract(Number(num1),Number(num2));
+    res.send(String(diff))
 })
 
 app.listen(3000, () => console.log("App has started"));
